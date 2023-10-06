5cd6966a92a415ecdb4d0e7474b14a051344f764
monzuralam <monzur707@gmail.com>
2023-10-06 12:01:14 +0600

    SettingForm has been added

--- src/backend/pages/Settings.vue
+++ src/backend/pages/Settings.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+  import SettingForm from '../components/SettingForm.vue';
+</script>
 
 <template>
   <h1 class="text-3xl text-black dark:text-white">Settings</h1>
+  <SettingForm />
 </template>
 
 <style scoped></style>
